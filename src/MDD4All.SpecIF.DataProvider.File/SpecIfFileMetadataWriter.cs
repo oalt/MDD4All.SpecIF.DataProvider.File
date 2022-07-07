e47a01b8789565dba6d4ad72116dc60135a89a11
Oliver Alt <oa@lacheweg21.de>
2022-07-07 23:44:09 +0200

    Add initial metadata writer

---
+++ src/MDD4All.SpecIF.DataProvider.File/SpecIfFileMetadataWriter.cs
@@ -0,0 +1,51 @@
+﻿using MDD4All.SpecIF.DataModels;
+using MDD4All.SpecIF.DataProvider.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MDD4All.SpecIF.DataProvider.File
+{
+    public class SpecIfFileMetadataWriter : ISpecIfMetadataWriter
+    {
+        public void AddDataType(DataType dataType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddPropertyClass(PropertyClass propertyClass)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddResourceClass(ResourceClass resourceClass)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddStatementClass(StatementClass statementClass)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateDataType(DataType dataType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePropertyClass(PropertyClass propertyClass)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateResourceClass(ResourceClass resourceClass)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateStatementClass(StatementClass statementClass)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
